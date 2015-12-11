ea040b330f431e2b59f27ea6f6d4f7c835470952
sonnguyenit <sonnguyen.it.1986@gmail.com>
2015-12-11 23:36:35 +0700

    u

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,25 @@
 window.onload = function() {
-    var converter = new showdown.Converter();
-    var pad = document.getElementById('pad');
+    var nsOption = {
+        strikethrough:true,
+        tables: true, 
+        tableHeaderId: true,
+        ghCodeBlocks: true,
+        tasklists: true,
+        simplifiedAutoLink: true,
+        parseImgDimensions: true
+    }
+    
+    var converter = new showdown.Converter(nsOption);
+    var pad = document.getElementById('ns-editor');
     var markdownArea = document.getElementById('markdown');   
 
     var convertTextAreaToMarkdown = function(){
         var markdownText = pad.value;
         html = converter.makeHtml(markdownText);
         markdownArea.innerHTML = html;
+        $('pre code').each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
     };
 
     pad.addEventListener('input', convertTextAreaToMarkdown);
